96744ee1e43604132ce8203cc8ed7387e5164ae6
YeonsuBaek <soothbugger@gmail.com>
2023-12-31 08:48:25 +0900

    📦  Update a package data

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom"
+import { TextComponent } from "./lib"
+import React from "react"
+
+ReactDOM.render(
+  <>
+    <TextComponent />
+  </>,
+  document.getElementById("root") as HTMLElement
+)
