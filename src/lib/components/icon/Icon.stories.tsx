8101d63285a517964d54dcb0b848461b57337db8
YeonsuBaek <soothbugger@gmail.com>
2024-01-03 18:54:19 +0900

    ✨ Add Icon Component to Storybook

---
+++ src/lib/components/icon/Icon.stories.tsx
@@ -0,0 +1,52 @@
+import type { ComponentStory, Meta } from "@storybook/react"
+import { Icon } from "."
+import { icons, IconType } from "./Icons"
+
+const meta: Meta<typeof Icon> = {
+  title: "Component/Icon",
+  component: Icon,
+}
+
+export default meta
+
+export const IconRender = () => {
+  const iconList: string[] = Object.keys(icons)
+  const renderIcons = iconList.map((icon: string) => (
+    <div
+      key={`icon-${icon}`}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Icon icon={icon as IconType} />
+      <span>{icon}</span>
+    </div>
+  ))
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
+      {renderIcons}
+    </div>
+  )
+}
+
+export const iconSizes = () => {
+  return (
+    <>
+      <Icon icon='Heart' size='small' />
+      <Icon icon='Heart' size='medium' />
+      <Icon icon='Heart' size='large' />
+    </>
+  )
+}
+
+export const iconColors = () => {
+  return (
+    <>
+      <Icon icon='Heart' color='#1890ff' />
+      <Icon icon='Heart' color='#722ed1' />
+      <Icon icon='Heart' color='#eb2f96' />
+    </>
+  )
+}
