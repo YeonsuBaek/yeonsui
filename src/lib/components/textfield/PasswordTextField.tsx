d6988dd852c7a892c3ac3d2c51573c8a1fc1287d
yeonsuBaek <soothbugger@gmail.com>
2024-02-07 13:05:42 +0900

    ✨ Implement PasswordTextField based on TextField

---
+++ src/lib/components/textfield/PasswordTextField.tsx
@@ -0,0 +1,30 @@
+import React, { useMemo, useState } from 'react'
+import { DefaultTextFieldProps } from '.'
+import { IconButton } from '../button'
+
+const PasswordTextField = ({ size = 'medium', disabled, id, value, onChange, placeholder }: DefaultTextFieldProps) => {
+  const [isShow, setIsShow] = useState(false)
+  const iconSize = useMemo(() => (size === 'large' ? 'medium' : 'small'), [size])
+
+  return (
+    <div className={`ui-textfield ${size} ${disabled ? 'disabled' : ''}`}>
+      <input
+        className="ui-textfield-input"
+        type={isShow ? 'text' : 'password'}
+        id={id}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+      />
+      <IconButton
+        icon={isShow ? 'EyeInvisible' : 'Eye'}
+        variant="secondary"
+        onClick={() => setIsShow((prev) => !prev)}
+        size={iconSize}
+      />
+    </div>
+  )
+}
+
+export default PasswordTextField
--- src/lib/components/textfield/index.tsx
+++ src/lib/components/textfield/index.tsx
@@ -2,13 +2,16 @@ import { ChangeEvent } from 'react'
 import TextField from './TextField'
 import { IconType } from '../icon/Icons'
 
-export interface TextFieldProps {
+export interface DefaultTextFieldProps {
   id: string
   value: string
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
   placeholder?: string
   size?: 'small' | 'medium' | 'large'
   disabled?: boolean
+}
+
+export interface TextFieldProps extends DefaultTextFieldProps {
   suffix?: string
   icon?: IconType
   type?: 'text' | 'password' | 'file'
