86fd8ac7aec61dcb38971677d5155d6b54f5e519
yeonsuBaek <soothbugger@gmail.com>
2024-02-07 11:32:48 +0900

    🚧 Add type props to Button

--- src/lib/components/button/Button.tsx
+++ src/lib/components/button/Button.tsx
@@ -1,35 +1,24 @@
-import React, { useMemo } from "react"
-import { ButtonProps } from "."
-import { Icon } from "../icon"
+import React, { useMemo } from 'react'
+import { ButtonProps } from '.'
+import { Icon } from '../icon'
 
 const Button = ({
   children,
-  variant = "primary",
-  size = "medium",
+  variant = 'primary',
+  size = 'medium',
   disabled = false,
   startIcon,
   endIcon,
   onClick,
+  type = 'button',
 }: ButtonProps) => {
-  const iconSize = useMemo(
-    () => (size === "large" ? "small" : "xsmall"),
-    [size]
-  )
+  const iconSize = useMemo(() => (size === 'large' ? 'small' : 'xsmall'), [size])
 
   return (
-    <button
-      type='button'
-      className={`ui-button ${variant} ${size}`}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      {startIcon && (
-        <Icon className='ui-button-icon' icon={startIcon} size={iconSize} />
-      )}
+    <button type={type} className={`ui-button ${variant} ${size}`} disabled={disabled} onClick={onClick}>
+      {startIcon && <Icon className="ui-button-icon" icon={startIcon} size={iconSize} />}
       <span>{children}</span>
-      {endIcon && (
-        <Icon className='ui-button-icon' icon={endIcon} size={iconSize} />
-      )}
+      {endIcon && <Icon className="ui-button-icon" icon={endIcon} size={iconSize} />}
     </button>
   )
 }
